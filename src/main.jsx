d8da3837dba648ee786b1061d432843b53e197ed
Ankit <ankit26kushwa@gmail.comm>
2023-06-06 13:02:03 +0530

    Add style in TodoItem

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import '@/styles/app.css';
 import TodoApp from '@/components/TodoApp';
 
 const domContainer = document.getElementById("root");
