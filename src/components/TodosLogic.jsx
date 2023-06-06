ce5583d45e0ba1fb99caaba6b62ad97ad96105aa
Ankit <ankit26kushwa@gmail.comm>
2023-06-06 13:14:32 +0530

    Add items in LocalStorage

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,26 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import InputTodo from '@/components/InputTodo';
 import TodosList from '@/components/TodosList';
 import { v4 as uuidv4 } from 'uuid';
 
 const TodosLogic = () => {
-    const [todos, setTodos] = useState([
-        {
-            id: uuidv4(),
-            title: 'Setup development environment',
-            completed: true,
-          },
-          {
-            id: uuidv4(),
-            title: 'Develop website and add content',
-            completed: false,
-          },
-          {
-            id: uuidv4(),
-            title: 'Deploy to live server',
-            completed: false,
-          },
-    ]);
+    const [todos, setTodos] = useState(getInitialTodos());
+
+  function getInitialTodos() {
+    // getting stored items
+    const temp = localStorage.getItem('todos');
+    const savedTodos = JSON.parse(temp);
+    return savedTodos || [];
+  }
+
+  useEffect(() => {
+    // storing todos items
+    const temp = JSON.stringify(todos);
+    localStorage.setItem('todos', temp);
+  }, [todos]);
     const handleChange = (id) => {
         setTodos((prevState) =>
           prevState.map((todo) => {
@@ -49,12 +46,21 @@ const TodosLogic = () => {
           };
           setTodos([...todos, newTodo]);
       }
+      const setUpdate = (updatedTitle, id) => {
+        setTodos(
+          todos.map((todo) => {
+            if (todo.id === id) {
+              todo.title = updatedTitle;
+            }
+            return todo;
+          })
+        );
+      };
     return (
         <div>
             <InputTodo addTodoItem={addTodoItem} />
-            <TodosList todosProps={todos} handleChange={handleChange} delTodo={delTodo} />
+            <TodosList todosProps={todos} handleChange={handleChange} delTodo={delTodo} setUpdate={setUpdate} />
         </div>
     )
   }
   export default TodosLogic;
-  
