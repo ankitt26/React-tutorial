591805c88657bbf98739cb63e5f840a50ae6d9fd
Ankit <ankit26kushwa@gmail.comm>
2023-06-06 13:11:13 +0530

    Add react icon in input

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
+import { FaPlusCircle } from "react-icons/fa"
+
 
 const InputTodo = ({ addTodoItem }) => {
     const [title, setTitle] = useState("");
     const [message, setMessage] = useState("");
-
     const handleChange = (e) => {
         setTitle(e.target.value);
     }
@@ -21,11 +22,13 @@ const InputTodo = ({ addTodoItem }) => {
         <>
         <form onSubmit={handleSubmit} className="form-container" >
       <input className="input-text" type="text" placeholder="Add Todo..." value={title} onChange={handleChange} />
-      <button className="input-submit">Submit</button>
+      <button className="input-submit">
+  <FaPlusCircle />
+</button>
+
     </form>
     <span className="submit-warning">{message}</span>
         </>
     )
   };
   export default InputTodo;
-  
